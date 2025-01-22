c880f5db193a08d439249691def04bd417d23dd4
codeuser-crypto <vijayshreyank@gmail.com>
2025-01-22 20:27:37 +0530

    committed

---
+++ 8. for loop.py
@@ -0,0 +1,18 @@
+name = 'bandi'
+for i in name:
+    print(i)
+    if(i =="b"):
+      print("This is something special!")
+    
+colors = ["Red", "Green"]
+for color in colors:
+    print(color)
+    for i in color:
+      print(i)
+
+for k in range(5):
+    print(k)
+
+  
+for k in range(1, 12, 3):
+    print(k)
